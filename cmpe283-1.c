446de9904e4b6651b82a90cd474ab8150c4d2c27
SAILAHARI SEETHAMRAJU <99698941+slahari5@users.noreply.github.com>
2022-11-04 14:07:40 -0700

    Adding Procbased controls Primary

--- cmpe283-1.c
+++ cmpe283-1.c
@@ -12,6 +12,11 @@
  * See SDM volume 4, section 2.1
  */
 #define IA32_VMX_PINBASED_CTLS	0x481
+#define IA32_VMX_PROCBASED_CTLS 	0x482
+#define IA32_VMX_PROCBASED_CTLS2	0x48B
+#define IA32_VMX_EXIT_CTLS		0x483
+#define IA32_VMX_ENTRY_CTLS		0x484
+#define IA32_VMX_PROCBASED_CTLS3	0x492
 
 /*
  * struct caapability_info
@@ -37,6 +42,37 @@ struct capability_info pinbased[5] =
 	{ 6, "Activate VMX Preemption Timer" },
 	{ 7, "Process Posted Interrupts" }
 };
+
+/*
+Proc-Based controls Primary
+*/
+struct capability_info procprimary[22] =
+{
+	{ 2, "Interrupt-window exiting" },
+	{ 3, "Use TSC offsetting" },
+	{ 7, "HLT exiting" },
+	{ 9, "INVLPG exiting" },
+	{ 10, "MWAIT exitin" },
+	{ 11, "RDPMC exiting"},
+	{ 12, "RDTSC exiting"},
+	{ 15, "CR3-load exiting"},
+	{ 16, "CR3-store exiting"},
+	{ 17, "Activate tertiary controls"},
+	{ 19, "CR8-load exiting"},
+	{ 20, "CR8-store exiting"},
+	{ 21, "Use TPR shadow"},
+	{ 22, "NMI-window exiting"},
+	{ 23, "MOV-DR exiting"},
+	{ 24, "Unconditional I/O exiting"},
+	{ 25, "Use I/O bitmaps"},
+	{ 27, "Monitor trap flag"},
+	{ 28, "Use MSR bitmaps"},
+	{ 29, "MONITOR exiting"},
+	{ 30, "PAUSE exiting"},
+	{ 31, "Activate secondary controls"}
+
+};
+
 
 /*
  * report_capability

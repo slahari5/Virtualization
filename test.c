910bf41025efe9e031b730bf2bb58bfccbfb2479
SAILAHARI SEETHAMRAJU <99698941+slahari5@users.noreply.github.com>
2022-12-08 23:44:52 -0800

    Update test.c

--- test.c
+++ test.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
-#include <sys/types.h>
+                #include <sys/types.h>
 
-static inline void
-__cpuid(unsigned int *eax, unsigned int *ebx, unsigned int *ecx,
-unsigned int *edx)
-{
-asm volatile("cpuid"
-: "=a" (*eax),
-"=b" (*ebx),
-"=c" (*ecx),
-"=d" (*edx)
-: "0" (*eax), "1" (*ebx), "2" (*ecx), "3" (*edx));
-}
 
-int
-main(int argc, char **argv)
-{
-unsigned int eax, ebx, ecx, edx;
-unsigned long long time;
+                static inline void
+                __cpuid(unsigned int *eax, unsigned int *ebx, unsigned int *ecx,
+                unsigned int *edx)
+                {
+                    asm volatile("cpuid"
+                    : "=a" (*eax),"=b" (*ebx),"=c" (*ecx),"=d" (*edx)
+                    : "0" (*eax), "1" (*ebx), "2" (*ecx), "3" (*edx));
+                }
 
-eax = 0x4FFFFFFC;
-__cpuid(&eax, &ebx, &ecx, &edx);
-printf("CPUID(0x4FFFFFFC), exits=%u", eax,);
-eax = 0x4FFFFFFD;
-__cpuid(&eax, &ebx, &ecx, &edx);
-printf("CPUID(0x4FFFFFFD), time in ebx: %u", ebx,);
-printf("CPUID(0x4FFFFFFD), time in ecx: %u", ecx,);
-}
+                int
+                main(int argc, char **argv)
+                {
+                    unsigned int eax, ebx, ecx, edx;
+                    unsigned long long cycle_time;
+
+                    eax = 0x4FFFFFFC;
+                    __cpuid(&eax, &ebx, &ecx, &edx);
+                    printf("CPUID(0x4FFFFFFC), Total exit counter = %u \n", eax);
+
+                    eax = 0x4FFFFFFD;
+                    __cpuid(&eax, &ebx, &ecx, &edx);
+                    cycle_time = (unsigned long long) ebx << 32 | ecx;
+                    printf("CPUID(0x4FFFFFFD), Total exit cycles = %llu \n", cycle_time);
+                }
